70b3d77c993db845136c698fdbe7c0b6f48477e0
LenarSag <saglenar@gmail.com>
2024-05-07 09:05:01 +0300

    initial commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,10 +21,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
     "users.apps.UsersConfig",
-
     "api.apps.ApiConfig",
-    'reviews.apps.ReviewsConfig',
+    "reviews.apps.ReviewsConfig",
 ]
 
 MIDDLEWARE = [
