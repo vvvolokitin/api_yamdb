b135f07f3df9152d0eaa2ceeb4d82e05117cec1c
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-05-10 07:55:55 +0500

    Fix pep

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-
 from .models import MyUser
 
 
 class MyUserAdmin(UserAdmin):
     model = MyUser
     list_display = (
-        "username", "email", "first_name", "last_name", "bio", "role"
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+        'role',
     )
-    list_editable = ("role",)
+    list_editable = ('role',)
     fieldsets = UserAdmin.fieldsets + (
-        ("Extra Fields", {"fields": ("bio", "role")}),
+        ('Extra Fields', {'fields': ('bio', 'role')}),
     )
 
 
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,39 +2,45 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
 
-from core.constants import MAX_USER_NAME_LENGTH, MAX_EMAIL_LENGTH
+from core.constants import MAX_EMAIL_LENGTH, MAX_USER_NAME_LENGTH
 
 
 class MyUser(AbstractUser):
     class UserRole(models.TextChoices):
-        USER = "user"
-        MODERATOR = "moderator"
-        ADMIN = "admin"
+        USER = 'user'
+        MODERATOR = 'moderator'
+        ADMIN = 'admin'
 
     username = models.CharField(
         max_length=150,
-        verbose_name="имя пользователя",
+        verbose_name='имя пользователя',
         unique=True,
         validators=[
             RegexValidator(
-                regex=r"^[\w.@+-]+$",
-                message="Недопустимый символ в имя пользователя"
+                regex=r'^[\w.@+-]+$',
+                message='Недопустимый символ в имя пользователя'
             )
         ],
     )
     email = models.EmailField(
-        max_length=MAX_EMAIL_LENGTH, unique=True, verbose_name="email"
+        max_length=MAX_EMAIL_LENGTH,
+        unique=True,
+        verbose_name='email'
     )
     first_name = models.CharField(
-        max_length=MAX_USER_NAME_LENGTH, verbose_name="имя", blank=True
+        max_length=MAX_USER_NAME_LENGTH,
+        verbose_name='имя',
+        blank=True
     )
     last_name = models.CharField(
-        max_length=MAX_USER_NAME_LENGTH, verbose_name="фамилия", blank=True
+        max_length=MAX_USER_NAME_LENGTH,
+        verbose_name='фамилия',
+        blank=True
     )
-    bio = models.TextField(verbose_name="биография", blank=True)
+    bio = models.TextField(verbose_name='биография', blank=True)
     role = models.CharField(
         max_length=20,
-        verbose_name="роль",
+        verbose_name='роль',
         choices=UserRole.choices,
         default=UserRole.USER,
     )
