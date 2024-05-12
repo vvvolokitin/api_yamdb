6eae032c124e04a7ab6b34b22f97d2ce6890e813
Андрей Фоминых <fominykhandrey88@mail.ru>
2024-05-12 23:48:10 +0500

    последний

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
@@ -174,18 +175,20 @@ class ReviewSerializer(serializers.ModelSerializer):
         slug_field='username', read_only=True)
     title = serializers.SlugRelatedField(slug_field='name', read_only=True)
 
+    def validate(self, data):
+        """Проверка на наличие отзыва."""
+        request = self.context.get('request')
+        author = request.user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (request.method == 'POST' and Review.objects.filter(
+                title=title, author=author).exists()):
+            raise serializers.ValidationError(
+                'Вы уже оставили отзыв на это произведение'
+            )
+        return data
+
     class Meta:
         model = Review
         fields = ('id', 'author', 'title', 'text', 'score', 'pub_date')
         read_only_fields = ('author', 'title')
-
-        def validate(self, data):
-            """Проверка на наличие отзыва."""
-            if Review.objects.filter(
-                    author=self.context['request'].user,
-                    title_id=self.context['view'].kwargs.get('title_id')
-            ).exists():
-                raise serializers.ValidationError(
-                    'Отзыв уже существует'
-                )
-            return data
