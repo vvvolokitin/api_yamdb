1f6b6e16185208be9b01498a4d00ba04ef09aaa4
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-05-14 22:06:46 +0500

    Fix

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -166,19 +166,6 @@ class ReviewSerializer(serializers.ModelSerializer):
         # default=''
     )
 
-    # def validate(self, data):
-    #     """Проверка на наличие отзыва."""
-    #     request = self.context.get('request')
-    #     author = request.user
-    #     title_id = self.context.get('view').kwargs.get('title_id')
-    #     title = get_object_or_404(Title, pk=title_id)
-    #     if (request.method == 'POST' and Review.objects.filter(
-    #             title=title, author=author).exists()):
-    #         raise serializers.ValidationError(
-    #             'Вы уже оставили отзыв на это произведение'
-    #         )
-    #     return data
-
     class Meta:
         model = Review
         fields = (
@@ -189,10 +176,23 @@ class ReviewSerializer(serializers.ModelSerializer):
             'score',
             'pub_date'
         )
-        validators = [
-            UniqueTogetherValidator(
-                queryset=model.objects.all(),
-                fields=['author', 'title'],
-                message='Вы уже оставили отзыв на это произведение'
+        # validators = [
+        #     UniqueTogetherValidator(
+        #         queryset=model.objects.all(),
+        #         fields=['author', 'title'],
+        #         message='Вы уже оставили отзыв на это произведение'
+        #     )
+        # ]
+
+    def validate(self, data):
+        """Проверка на наличие отзыва."""
+        request = self.context.get('request')
+        author = request.user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (request.method == 'POST' and Review.objects.filter(
+                title=title, author=author).exists()):
+            raise serializers.ValidationError(
+                'Вы уже оставили отзыв на это произведение'
             )
-        ]
+        return data
